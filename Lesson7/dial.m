27d113323a675f96bec0c37ba50f8ad90b097a7c
eyllanesc <e.yllanescucho@ieee.org>
2015-05-22 15:27:39 -0500

    Lesson7++

--- Lesson7/dial.m
+++ Lesson7/dial.m
@@ -1,4 +1,20 @@
-function [A] = dial(input)
-A = char(1:122);
-A('A':'Z') = str2num(A('A':'Z')-'A'+'0');
-end
+function [output] = dial(input)
+data = ['ABC'; 'DEF'; 'GHI'; 'JKL'; 'MNO'; 'PRS'; 'TUV'; 'WXY'];
+output = input;
+    for k = 1:length(input)
+        if input(k)< '0' || input(k)>'9'
+            [p, q] = find(data==input(k));
+            if ~isempty(p)
+                output(k) = num2str(p+1);
+            elseif input(k)=='#' || input(k)=='*'
+                output(k) = input(k);
+            elseif input(k)=='(' || input(k)==')' || input(k)=='-' || input(k)== ' '
+                output(k) = ' ';
+            else
+                output = [];
+                break;
+            end
+        else
+            output(k) = input(k);
+        end
+    end
--- Lesson7/roman.m
+++ Lesson7/roman.m
@@ -1,1 +1,11 @@
 function [out] = roman(in)
+len = length(in);
+if len > 1
+    out = 1;
+else
+    for k= 1:len-1
+        if strcmp(in(k:k+1), 'IV');
+            
+        end
+    end
+end
